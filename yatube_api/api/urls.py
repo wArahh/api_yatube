f3ba8eff75015189b54ccf832715401bd379bff8
Nikita <nikitkamakarenko@gmail.com>
2024-01-17 18:44:19 +0300

    router added

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import include, path
 from rest_framework.authtoken import views
+from rest_framework.routers import SimpleRouter
+from .views import PostViewSet
+
+router = SimpleRouter()
+router.register('posts', PostViewSet)
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('api/v1/', include(router.urls))
 ]
