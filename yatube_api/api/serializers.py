acd98fd000544ea946e9cf0eb68de4d4cca9e3ad
Nikita <nikitkamakarenko@gmail.com>
2024-01-19 22:20:34 +0300

    pytested

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,10 +1,19 @@
+from posts.models import Comment, Group, Post
 from rest_framework import serializers
-from posts.models import Post, Group, Comment
+
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
     class Meta:
         model = Post
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['author'] = self.context['request'].user
+        return super().create(validated_data)
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -14,6 +23,11 @@ class GroupSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ['author', 'post']
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,26 +1,67 @@
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from posts.models import Comment, Group, Post
+from rest_framework import generics, status, viewsets
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-from rest_framework import generics
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
-from posts.models import Group, Post, Comment
 
-from .serializers import PostSerializer, GroupSerializer, CommentSerializer
-
+@permission_classes([IsAuthenticated])
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def update(self, request, *args, **kwargs):
+        post = self.get_object()
+        if post.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
 
+    def destroy(self, request, *args, **kwargs):
+        post = self.get_object()
+        if post.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+
+@permission_classes([IsAuthenticated])
 class GroupList(generics.ListCreateAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+    def post(self, request, *args, **kwargs):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+
+@permission_classes([IsAuthenticated])
 class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
+@permission_classes([IsAuthenticated])
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        return Comment.objects.filter(post=post)
+
+    def update(self, request, *args, **kwargs):
+        post = self.get_object()
+        if post.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return super().destroy(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs['post_id'])
+        serializer.save(author=self.request.user, post=post)
