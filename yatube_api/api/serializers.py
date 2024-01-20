f716f1eb931a80f83cd85e9ef151035b077236a2
Nikita <nikitkamakarenko@gmail.com>
2024-01-20 18:52:13 +0300

    ya genii

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -27,4 +27,4 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
-        read_only_fields = ['post']
+        read_only_fields = ('post',)
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -9,56 +9,43 @@ from posts.models import Comment, Group, Post
 
 
 @permission_classes([IsAuthenticated])
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
+class BaseViewSet(viewsets.ModelViewSet):
     def update(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != self.request.user:
+        instance = self.get_object()
+        if instance.author != self.request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
         return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != self.request.user:
+        instance = self.get_object()
+        if instance.author != self.request.user:
             return Response(status=status.HTTP_403_FORBIDDEN)
         return super().destroy(request, *args, **kwargs)
+
+
+class PostViewSet(BaseViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-@permission_classes([IsAuthenticated])
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-@permission_classes([IsAuthenticated])
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all()
+class CommentViewSet(BaseViewSet):
     serializer_class = CommentSerializer
 
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs['post_id'])
+
     def get_queryset(self):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
-        return Comment.objects.filter(post=post)
-
-    def update(self, request, *args, **kwargs):
-        post = self.get_object()
-        if post.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        comment = self.get_object()
-        if comment.author != self.request.user:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        return super().destroy(request, *args, **kwargs)
+        return Comment.objects.filter(post=self.get_post())
 
     def perform_create(self, serializer):
-        post = get_object_or_404(Post, id=self.kwargs['post_id'])
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
+
